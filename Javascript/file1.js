ebc869812f3a7d483c987289eaf76b75109fd2db
Vansh6401 <Vanshsharma6401@gmail.com>
2025-06-20 14:37:58 +0530

    update

--- Javascript/file1.js
+++ Javascript/file1.js
@@ -1,4 +1,40 @@
-let a = 10
-let b = 20
-let c = a+b
-console.log(c)
+//Introduction to JavaScript
+
+// let studentName = "john"
+// let age = 20
+// let promoted = true
+// let score
+// let city = null
+// console.log(studentName)
+// console.log(typeof studentName)
+// console.log(typeof age)
+// console.log(typeof promoted)
+// console.log(typeof score)
+// console.log(typeof city)
+
+
+// let age = "10"
+// console.log(typeof age)
+// let ageNum = Number(age)
+// console.log(typeof ageNum)
+
+// let a = "10"
+// let b = 10
+// console.log(a+b)  //1010
+// console.log(a*b)  //100 javascript convert string "10" to Number and multiply
+
+// let a = 2.10
+// let b = 2.10
+// console.log(a===b)
+
+// const c1 = "LPU"
+// c1 = "cu"
+// console.log(c1)
+
+/*
+var stName = "John"
+console.log(stName)
+*/
+
+let stname = "Khushboo"
+console.log(`My name is ${stname}`)
