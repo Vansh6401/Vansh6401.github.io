037aabafa5671af087831096363d4b6f72d7eda8
Vansh6401 <Vanshsharma6401@gmail.com>
2025-06-23 16:56:20 +0530

    Update

---
+++ Javascript/file11.js
@@ -0,0 +1,9 @@
+//import export
+
+import add,{subtract} from "./calc.mjs"
+
+const result = add(4, 5);
+console.log(result);
+
+const result1 = subtract(4,1);
+console.log(result1);
---
+++ Javascript/file13.js
@@ -0,0 +1,10 @@
+//Spread
+let marks = {
+    maths: 95,
+};
+marks = {...marks, science: 54};
+console.log(marks)
+
+let students = ["harsh", "Vansh", "UDD"];
+students = [...students, "Aditya"];
+console.log(students)
